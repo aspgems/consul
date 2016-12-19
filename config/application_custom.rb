e9b1263fe560aabd4aa19c371eb117b0318ef898
Juanma <juanma@aspgems.com>
2016-12-19 17:32:44 +0100

    Added smtp configuration to .yml

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,5 +1,6 @@
 module Consul
   class Application < Rails::Application
     config.consul = config_for(:consul)
+    config.action_mailer.smtp_settings = config.consul['smtp'].symbolize_keys if config.consul.key?('smtp')
   end
 end
