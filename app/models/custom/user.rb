6d6fb313097df598c07632d5b733d6ed13c52c02
fandres <fandrod@yahoo.es>
2017-01-05 12:06:45 +0100

    [Resolves #TGi17] Add phone number as editable field on user account page

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -1,7 +1,7 @@
 require_dependency Rails.root.join('app', 'models', 'user').to_s
 
 class User
-  validates :phone_number, format: { with: /\A[\d \+]+\z/ }
+  validates :phone_number, phone: { allow_blank: true, types: [:mobile, :fixed_line] }
 
   scope :unverified, -> { where(verified_at: nil).where.not(document_number: nil) }
   scope :search_by_email_username_document, ->(term) { where("email = ? OR username ILIKE ? OR document_number ILIKE ?", term, "%#{term}%", "%#{term}%") }
