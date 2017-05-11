0a1f8763dea23a8e733425138e6d67ebf2c4e535
Miguel Angel Caro <mcaro@aspgems.com>
2017-05-11 12:03:08 +0200

    Changes in routing config and params names

--- app/controllers/custom/admin/verifications_controller.rb
+++ app/controllers/custom/admin/verifications_controller.rb
@@ -2,6 +2,7 @@ require_dependency Rails.root.join('app', 'controllers', 'admin', 'verifications
 
 class Admin::VerificationsController < Admin::BaseController
 
+  # The controller name is correct?? Should be user_unverified_controller?.
   def index
     @users = User.unverified.page(params[:page]).for_render
   end
@@ -11,15 +12,16 @@ class Admin::VerificationsController < Admin::BaseController
     render :index
   end
 
+  # Create and destroy methods should be here? Is the correct place?.
   def create
     @user = User.unverified.find(params[:user_id])
     @user.update(verified_at: Time.now) if @user
-    redirect_to search_admin_verifications_path(request.query_parameters.dup.extract!(:term, :page)),
+    redirect_to search_admin_verifications_path(request.query_parameters.slice(:term, :page)),
                 notice: t('admin.settings.flash.updated')
   end
 
   def destroy
-    @user = User.unverified.find(params[:user_id])
+    @user = User.unverified.find(params[:id])
     @user.destroy_verification
     redirect_to search_admin_verifications_path(request.query_parameters.slice(:term, :page)),
                 notice: t('admin.settings.flash.updated')
