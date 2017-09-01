785d5801178541172fe4a5419ac00499c2d59be5
Ricardo Perez <reperezz@gmail.com>
2017-09-01 11:47:25 +0200

    Add email to report account verification

--- app/controllers/custom/admin/verifications_controller.rb
+++ app/controllers/custom/admin/verifications_controller.rb
@@ -13,7 +13,10 @@ class Admin::VerificationsController < Admin::BaseController
 
   def create
     @user = User.unverified.find(params[:user_id])
-    @user.update(verified_at: Time.now) if @user
+    if @user
+      @user.update(verified_at: Time.now)
+      Mailer.account_verification(@user, @user.email).deliver_later
+    end
     redirect_to search_admin_verifications_path(request.query_parameters.dup.extract!(:term, :page)),
                 notice: t("admin.settings.flash.updated")
   end
