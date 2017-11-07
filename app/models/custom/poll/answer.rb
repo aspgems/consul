d1196929a58acd5b263d1252ac6d179af3e7f975
Jose Miguel Díez de la Lastra <jmdiez@aspgems.com>
2017-11-07 11:28:20 +0100

    only changed method in models

--- app/models/custom/poll/answer.rb
+++ app/models/custom/poll/answer.rb
@@ -1,19 +1,9 @@
+require_dependency Rails.root.join('app', 'models', 'poll', 'answer').to_s
+
 class Poll::Answer < ActiveRecord::Base
-
-  belongs_to :question, -> { with_hidden }
-  belongs_to :author, ->   { with_hidden }, class_name: 'User', foreign_key: 'author_id'
-
-  delegate :poll, :poll_id, to: :question
-
-  validates :question, presence: true
-  validates :author, presence: true
-  validates :answer, presence: true
-  validates :answer, inclusion: {in: ->(a) { a.question.valid_answers }}
-
-  scope :by_author, ->(author_id) { where(author_id: author_id) }
-  scope :by_question, ->(question_id) { where(question_id: question_id) }
 
   def record_voter_participation
     Poll::Voter.find_or_create_by(user: author, poll: poll)
   end
+
 end
