327a1d317372a26b106dd214f1b9f3bdbadbbc45
Ricardo Perez <reperezz@gmail.com>
2017-11-15 13:59:25 +0100

    Delete custom answer

--- app/models/custom/poll/answer.rb
+++
@@ -1,9 +0,0 @@
-require_dependency Rails.root.join('app', 'models', 'poll', 'answer').to_s
-
-class Poll::Answer < ActiveRecord::Base
-
-  def record_voter_participation
-    Poll::Voter.find_or_create_by(user: author, poll: poll)
-  end
-
-end
